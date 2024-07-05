82a0f2bf0d63e4bdd289763cbac7f9bbf4fbfbc7
fabiangzvo <faguzman.97@gmail.com>
2024-07-05 14:29:08 -0500

    add responsive design in interest section

---
+++ src/components/interest/constants.ts
@@ -0,0 +1,38 @@
+import { Interest } from "./types";
+
+const interests: Interest[] = [
+  {
+    title: "Senderismo",
+    description:
+      "Explorar la naturaleza es mi escape perfecto. Desde las alturas de las montañas hasta los senderos más serenos, encuentro paz y rejuvenecimiento en cada paso que doy.",
+  },
+  {
+    title: "Viajar",
+    description:
+      "Descubrir nuevos horizontes alimenta mi alma aventurera. Cada destino es una oportunidad para aprender, crecer y encontrar inspiración en culturas diversas y paisajes impresionantes.",
+  },
+  {
+    title: "Running",
+    description:
+      "Correr libera mi mente y fortalece mi espíritu. En cada zancada encuentro energía y determinación para superar retos, alcanzar metas y mantenerme en equilibrio físico y mental.",
+  },
+  {
+    title: "Training",
+    description:
+      "El gimnasio es mi santuario de fuerza y bienestar. Aquí encuentro la motivación para desafiarme físicamente y mantener un estilo de vida activo que complementa mi pasión por el diseño gráfico y la creatividad.",
+  },
+  {
+    title: "Pintar",
+    description:
+      "El arte es mi refugio creativo. A través del pincel y los colores, puedo expresar emociones y capturar momentos que inspiran mi diseño gráfico.",
+  },
+  {
+    title: "Natación",
+    description:
+      "Sumergirme en el agua me brinda una sensación de libertad y serenidad, además de ser una excelente forma de mantenerme en forma y saludable.",
+  },
+];
+
+const constants = { interests };
+
+export default constants;
--- src/components/interest/index.tsx
+++ src/components/interest/index.tsx
@@ -1,69 +1,36 @@
-import { JSX } from "react";
+"use client";
+
+import { JSX, useMemo } from "react";
 import Image from "next/image";
 
 import styles from "./styles.module.css";
+import constants from "./constants";
 
 function Interest(): JSX.Element {
+  const { first, second } = useMemo(() => {
+    const size = constants.interests.length;
+    const items = constants.interests.map((interest, index) => (
+      <section key={index} className={styles.item}>
+        <h3 className={styles.label}>{interest.title}</h3>
+        <p className={styles.paragraph}>{interest.description}</p>
+      </section>
+    ));
+
+    return {
+      first: items.slice(0, size / 2),
+      second: items.slice(size / 2, size),
+    };
+  }, []);
+
   return (
     <div id="interests" className={styles.container}>
       <span className={styles.title}>
         Mis&nbsp;<p className={styles.name}>intereses</p>
       </span>
       <div className={styles.content}>
-        <div className={styles.section}>
-          <section>
-            <h3 className={styles.label}>Senderismo</h3>
-            <p className={styles.paragraph}>
-              Explorar la naturaleza es mi escape perfecto. Desde las alturas de
-              las montañas hasta los senderos más serenos, encuentro paz y
-              rejuvenecimiento en cada paso que doy.
-            </p>
-          </section>
-          <section>
-            <h3 className={styles.label}>Viajar</h3>
-            <p className={styles.paragraph}>
-              Descubrir nuevos horizontes alimenta mi alma aventurera. Cada
-              destino es una oportunidad para aprender, crecer y encontrar
-              inspiración en culturas diversas y paisajes impresionantes.
-            </p>
-          </section>
-          <section>
-            <h3 className={styles.label}>Running</h3>
-            <p className={styles.paragraph}>
-              Correr libera mi mente y fortalece mi espíritu. En cada zancada
-              encuentro energía y determinación para superar retos, alcanzar
-              metas y mantenerme en equilibrio físico y mental.
-            </p>
-          </section>
-        </div>
+        <div className={styles.section}>{first}</div>
         <Image src="/interest.png" alt="user" width={60} height={500} />
-        <div className={styles.section}>
-          <section>
-            <h3 className={styles.label}>Training</h3>
-            <p className={styles.paragraph}>
-              El gimnasio es mi santuario de fuerza y bienestar. Aquí encuentro
-              la motivación para desafiarme físicamente y mantener un estilo de
-              vida activo que complementa mi pasión por el diseño gráfico y la
-              creatividad.
-            </p>
-          </section>
-          <section>
-            <h3 className={styles.label}>Pintar</h3>
-            <p className={styles.paragraph}>
-              El arte es mi refugio creativo. A través del pincel y los colores,
-              puedo expresar emociones y capturar momentos que inspiran mi
-              diseño gráfico.
-            </p>
-          </section>
-          <section>
-            <h3 className={styles.label}>Natación</h3>
-            <p className={styles.paragraph}>
-              Sumergirme en el agua me brinda una sensación de libertad y
-              serenidad, además de ser una excelente forma de mantenerme en
-              forma y saludable.
-            </p>
-          </section>
-        </div>
+        <div className={styles.section}>{second}</div>
       </div>
     </div>
   );
