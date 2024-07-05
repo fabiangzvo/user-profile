47f01be7dea4fad9d671d73c6f11c892eba2f634
fabiangzvo <faguzman.97@gmail.com>
2024-07-05 14:59:42 -0500

    add responsive design in contact form

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,17 +1,28 @@
 "use client";
 
-import { JSX, useCallback, SyntheticEvent } from "react";
+import { JSX, useCallback, SyntheticEvent, useState } from "react";
 
 import Input from "@components/input";
 
 import styles from "./styles.module.css";
 
 function Contact(): JSX.Element {
+  const [statusMessage, setStatusMessage] = useState("");
+
   const handleSubmit = useCallback((e: SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const form = e.currentTarget;
 
-    console.log(form.elements);
+    const form = e.currentTarget;
+    const inputName = form.elements[0] as HTMLInputElement;
+
+    setStatusMessage(
+      `Hola ${inputName.value}, he recibido tu mensaje y pronto estaré en contacto contigo.`
+    );
+
+    setTimeout(() => {
+      form.reset();
+      setStatusMessage("");
+    }, 50000);
   }, []);
 
   return (
@@ -25,9 +36,12 @@ function Contact(): JSX.Element {
           ¡Contáctame y juntos haremos que tu visión se destaque!
         </p>
         <form className={styles.form} onSubmit={handleSubmit}>
+          <label className={styles.statusMessage}>{statusMessage}</label>
+
           <Input label="Nombre" name="name" type="text" isRequired />
           <Input label="Correo" name="email" type="email" isRequired />
           <Input label="Mensaje" name="message" type="textarea" isRequired />
+
           <button type="submit" className={styles.button}>
             Enviar
           </button>
